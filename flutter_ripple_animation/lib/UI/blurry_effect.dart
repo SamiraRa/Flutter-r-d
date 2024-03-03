7cff14b30453458274abad309e911970da8d1c05
SamiraRa <53578188+SamiraRa@users.noreply.github.com>
2024-03-03 23:09:08 +0600

    flutter ripple animation

---
+++ flutter_ripple_animation/lib/UI/blurry_effect.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BlurryEffectScreen extends StatefulWidget {
+  const BlurryEffectScreen({super.key});
+
+  @override
+  State<BlurryEffectScreen> createState() => _BlurryEffectScreenState();
+}
+
+class _BlurryEffectScreenState extends State<BlurryEffectScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
