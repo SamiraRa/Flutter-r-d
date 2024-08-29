ab8ff75d88f045af2e008ab2b61d3d1dfef3b6f2
Samira <samira.rashid@transcombd.com>
2024-08-29 17:53:06 +0600

    videoplayer addded

--- flutter_object_box/lib/view/ppt_view.dart
+++ flutter_object_box/lib/view/ppt_view.dart
@@ -1,13 +1,12 @@
 import 'dart:async';
 import 'dart:io';
-
-import 'package:easy_docs_viewer/easy_docs_viewer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_pdfview/flutter_pdfview.dart';
 import 'package:file_picker/file_picker.dart';
 // import 'package:permission_handler/permission_handler.dart';
 import 'package:open_file/open_file.dart';
+import 'package:video_player/video_player.dart';
 
 class PptView extends StatefulWidget {
   const PptView({Key? key}) : super(key: key);
@@ -17,6 +16,7 @@ class PptView extends StatefulWidget {
 }
 
 class _PptViewState extends State<PptView> {
+  VideoPlayerController? _videoController;
   final Completer<PDFViewController> _controller = Completer<PDFViewController>();
   int? pages = 0;
   int? currentPage = 0;
@@ -28,9 +28,26 @@ class _PptViewState extends State<PptView> {
   @override
   void initState() {
     super.initState();
-    setState(() {
-      getFile();
-    });
+    _videoController = VideoPlayerController.networkUrl(
+        Uri.parse('https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4'))
+      ..addListener(() {
+        setState(() {});
+      })
+      ..initialize().then((_) {
+        setState(() {
+          _videoController!.play();
+        });
+        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
+      });
+    // setState(() {
+    //   getFile();
+    // });
+  }
+
+  @override
+  void dispose() {
+    _videoController!.dispose();
+    super.dispose();
   }
 
   void getFile() async {
@@ -40,11 +57,12 @@ class _PptViewState extends State<PptView> {
     final fileExt = file.extension;
     print(fileExt);
     if (fileExt == "pptx") {
-      pdfFilePath = file.path.toString();
+      // pdfFilePath = file.path.toString();
       // fromAsset("abcd", file).then((value) {
-      //   setState(() {
-      //     pdfFilePath = file.path.toString();
-      //   });
+      setState(() {
+        pdfFilePath = file.path.toString();
+        print("pdfFilepath $pdfFilePath");
+      });
       // });
     }
   }
@@ -64,18 +82,89 @@ class _PptViewState extends State<PptView> {
 
   @override
   Widget build(BuildContext context) {
+    final isMuted = _videoController!.value.volume == 0;
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("PPT Files"),
-          actions: <Widget>[
-            IconButton(
-              icon: const Icon(Icons.share),
-              onPressed: () {},
-            ),
-          ],
-        ),
-        body: Column(children: <Widget>[
-          SizedBox(width: double.infinity, height: 650, child: EasyDocsViewer(url: pdfFilePath))
-        ]));
+      appBar: AppBar(
+        title: const Text("PPT Files"),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.share),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Stack(
+        children: <Widget>[
+          _videoController != null && _videoController!.value.isInitialized
+              ? SizedBox(
+                  height: 300,
+                  child: AspectRatio(
+                    aspectRatio: _videoController!.value.aspectRatio,
+                    child: VideoPlayer(_videoController!),
+                  ),
+                )
+              : const SizedBox(),
+          _videoController != null && _videoController!.value.isInitialized
+              ? Positioned(
+                  bottom: 0, // Position at the bottom
+                  left: 0, // Ensure it stretches across the width
+                  right: 0,
+                  child: SizedBox(
+                      child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          InkWell(
+                            onTap: () {
+                              setState(() {
+                                _videoController!.value.isPlaying
+                                    ? _videoController!.pause()
+                                    : _videoController!.play();
+                              });
+                            },
+                            child: Padding(
+                              padding: const EdgeInsets.fromLTRB(0, 10, 10, 10),
+                              child: Icon(
+                                _videoController!.value.isPlaying ? Icons.pause : Icons.play_arrow,
+                                color: Colors.white,
+                                size: 30,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          InkWell(
+                            onTap: () {
+                              setState(() {
+                                _videoController!.setVolume(isMuted ? 1 : 0);
+                              });
+                            },
+                            child: Padding(
+                              padding: const EdgeInsets.fromLTRB(0, 10, 10, 10),
+                              child: Icon(
+                                _videoController!.value.volume != 0.0 ? Icons.volume_mute : Icons.volume_off,
+                                color: Colors.white,
+                                size: 30,
+                              ),
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  )))
+              : const SizedBox(),
+          _videoController != null && _videoController!.value.isInitialized
+              ? Positioned(
+                  bottom: 0, // Position at the bottom
+                  left: 0, // Ensure it stretches across the width
+                  right: 0,
+                  child: VideoProgressIndicator(_videoController!, allowScrubbing: true))
+              : const SizedBox()
+        ],
+      ),
+    );
   }
 }
--- flutter_object_box/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flutter_object_box/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,10 +9,12 @@ import device_info_plus
 import location
 import objectbox_flutter_libs
 import path_provider_foundation
+import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   ObjectboxFlutterLibsPlugin.register(with: registry.registrar(forPlugin: "ObjectboxFlutterLibsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
 }
